4b071ecb1c5e20b0b37fad6a73de4189f3b07d65
Bianca <BiEsteves2006@gmail.com>
2025-09-27 21:19:44 -0300

    tentando criar um Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }  // rota principal
+export const routes: Routes = [
+  // Rota padrão para o login
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  // Rota para o dashboard
+  { path: 'dashboard', component: DashboardComponent },
 ];
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router'; // 1. IMPORT OBRIGATÓRIO
 
 @Component({
   selector: 'app-login',
@@ -16,15 +17,25 @@ export class LoginComponent {
   successMessage: string = '';
   senhaIncorreta: boolean = false; 
 
-  login() {//Se o nome do usuario for igual a 'admin' é a senha igual a '12345' então, entrada confirmada
+
+  constructor(private router: Router) {}
+
+  login() {
+    // Se o nome do usuario for igual a 'admin' é a senha igual a '12345' então, entrada confirmada
     if (this.username.toLowerCase() === 'admin' && this.password === '12345') {
       this.successMessage = `✅ Bem-vindo, ${this.username}!`;
       this.errorMessage = '';
       this.senhaIncorreta = false;
       this.password = '';
     
-      //senão entrada não confirmada
+     
+      setTimeout(() => {
+        // Redireciona para o dashboard
+        this.router.navigate(['/dashboard']); 
+      }, 800); // dá um pequeno delay para mostrar a mensagem de sucesso
+
     } else {
+      // senão entrada não confirmada
       this.errorMessage = '⚠️ Usuário ou senha incorretos!';
       this.successMessage = '';
       this.password = '';
