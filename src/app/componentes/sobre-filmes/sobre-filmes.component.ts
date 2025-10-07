216ebc00b86e93b6255a1e1ea575c339db8b1170
Bianca <biesteves2006@gmail.com>
2025-10-07 15:31:04 -0300

    Modificações!

--- src/app/componentes/sobre-filmes/sobre-filmes.component.ts
+++ src/app/componentes/sobre-filmes/sobre-filmes.component.ts
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class SobreFilmesComponent implements OnInit {
   movieData: any;
   tipo: string = 'filme'; // filme, serie ou anime
-  private apiKey: string = '';
+  private apiKey: string = 'ADICIONE A CHAVE A API AQUI!!';//Chave API
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
